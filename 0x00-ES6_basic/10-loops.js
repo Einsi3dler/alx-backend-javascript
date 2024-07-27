e7c8f03c7aabad780fbb5dcd0fd2a6fbded122db
Sylvester Divine <sylvesterdivine@outlook.com>
2024-07-27 14:12:43 +0100

    random fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  for (const value of array) {
-    newArray.push(appendString + value);
+  const myarray = [];
+  for (const item of array) {
+    myarray.push(appendString + item);
   }
 
-  return newArray;
+  return myarray;
 }
